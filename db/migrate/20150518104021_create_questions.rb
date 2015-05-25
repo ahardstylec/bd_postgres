f4cc070bc182db1935e1dfc4b4dcc2e61bc6b0df
ahardstylec <andreas.collmann@gmail.com>
2015-05-25 19:08:16 +0200

    coommit

--- db/migrate/20150518104021_create_questions.rb
+++ db/migrate/20150518104021_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.text :question
       t.text :author_name
-      t.text :author_email, index: true
+      t.text :author_email
       t.integer :author_id
       t.text :flaws, array: true, default: []
     end
